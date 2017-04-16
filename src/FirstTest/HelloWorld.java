f25892df2879af26a8cd1e46f6bd2bc0c08d74ba
472308464@qq.com <472308464@qq>
2017-04-16 15:13:44 +0800

    测试与git链路

---
+++ src/FirstTest/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * Created by lenovo on 2017/4/16.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("删除ctrl+space增加代码提示功能，alt+/");
+        System.out.print("hello world");
+    }
+}
